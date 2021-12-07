d03c2fc65aa595fc85969facedc1cddee9067bef
over <overrimachi2001@gmail.com>
2021-12-07 14:55:11 -0500

    Version 3

---
+++ templates/otros/lenguajes.js
@@ -0,0 +1,16 @@
+let variable = 0
+const data = () => {
+
+    const lista = ["Python", "JavaScript", "Php", "Html", "Css"];
+    console.log(lista[variable]);
+
+    document.getElementById("lenguaje").innerHTML = lista[variable];
+    const imagen = document.getElementById("imagen");
+    imagen.setAttribute("src", "./static/images/" + lista[variable] + ".png")
+    variable = variable + 1;
+    if (variable === 5) {
+        variable = 0
+    }
+    // return lista;
+};
+const id = window.setInterval(data, 2000)
